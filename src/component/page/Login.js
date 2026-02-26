76e288cc106bc0ec34fbb23406658671b451f6ae
inkuk <sweetchild22.ik@gmail.com>
2026-02-26 16:53:25 +0900

    update

--- src/component/page/Login.js
+++ src/component/page/Login.js
@@ -3,17 +3,20 @@ import axios from 'axios';
 
 import AuthContext from "../util/AuthContext.js";
 import ProfileContext from "../util/ProfileContext.js";
-import React, { useContext, useState, useEffect} from 'react';
+import React, { useContext, useEffect, useState} from 'react';
 
 import * as api from '../util/Api.js'
 import * as blobToBase64 from '../util/BlobToBase64.js'
-import { BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import * as validator from '../util/Validator.js'
+
+import BeautyButton from '../common/BeautyButton.js';
 
 export default function() {
 
     const {auth, updateAuth, validAuth} = useContext(AuthContext)
     const {profile, updateProfile, removeProfile} = useContext(ProfileContext)
+    const [isLoading, setIsLoading] = useState(false)
     
     const navigate = useNavigate();
 
@@ -32,40 +35,40 @@ export default function() {
         
         const username = 'crazygun22@nate.com'
         const password = 'Sweetchild@22'
-                
-        if(!validator.email(username)){
+
+        if(username === ''){
             input_username.focus()
             return
         }
         
-        if(!validator.password(password)){
+        if(password === ''){
             input_password.focus()
             return
         }
 
-        btn_login.disabled = true
+        setIsLoading(true)
 
         const resAuth = await api.postAuthenticate(username, password)
         
         if(resAuth == null) {
-            btn_login.disabled = false
-            window.showToast('login 실패', 'error')
+            setIsLoading(false)            
+            window.showToast('로그인이 실패하였습니다', 'error')
             return
         }
         
         const resUser = await api.getUser(resAuth.jwt, resAuth.user_id)
         
         if(resUser == null) {
-            btn_login.disabled = false
-            window.showToast('login 실패', 'error')
+            setIsLoading(false)
+            window.showToast('로그인이 실패하였습니다', 'error')
             return
         }
 
-        if(resUser.profile == null){
-            btn_login.disabled = false
+        if(resUser.profile == null){            
             removeProfile()
             updateAuth(resAuth)
-            window.showToast('login 완료', 'success')
+            setIsLoading(false)
+            window.showToast('로그인이 성공하였습니다', 'success')
             return
         }
 
@@ -73,33 +76,42 @@ export default function() {
 
         const resProfile = await api.getProfile(resAuth.jwt, profileId)
 
-        btn_login.disabled = false
+        setIsLoading(false)
         
-        if(resProfile == null){            
-            window.showToast('login 실패', 'error')
+        if(resProfile == null){
+            window.showToast('로그인이 실패하였습니다', 'error')
             return
         }
-                
+
         const base64 = await blobToBase64.convert(resProfile)
         updateProfile(base64)
         updateAuth(resAuth)
-        window.showToast('login 완료', 'success')
+        window.showToast('로그인이 성공하였습니다', 'success')
     }
 
-    const onKeyDownEnter = async(event)=>{
+
+    const onKeyDownPassword = async(event)=>{
         if (event.key === 'Enter')
             await onClickLogin()
+    }
+
+
+    const onKeyDownUserName = async(event)=>{
+
+        if (event.key === 'Enter')
+            input_password.focus()
+
     }
     
 
     return !validAuth(auth) ? (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <label htmlFor='input_username'>Username</label>
-            <input id='input_username' type='text'/>
-            <label htmlFor='input_password'>Password</label>
-            <input id='input_password' type='password' onKeyDown={onKeyDownEnter}/>
-            <button id='btn_login' className='theme-btn' onClick={onClickLogin} >로그인</button>
-            <button id='btn_regist' onClick={() => {navigate('/regist', {replace:true})}}>회원가입</button>
+            <label htmlFor='input_username'>사용자 이름</label>
+            <input id='input_username' type='text' onKeyDown={onKeyDownUserName}/>
+            <label htmlFor='input_password'>비밀번호</label>
+            <input id='input_password' type='password' onKeyDown={onKeyDownPassword}/>
+            <BeautyButton onClick={onClickLogin}  isLoading={isLoading} type='success'>로그인</BeautyButton>
+            <BeautyButton onClick={() => {navigate('/regist', {replace:true})}}>회원가입</BeautyButton>
         </div>
     ) : null
 }

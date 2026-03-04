096d3574335d6b11769bc1dd9d65756782295c61
inkuk <sweetchild22.ik@gmail.com>
2026-03-04 12:27:31 +0900

    update

--- src/component/page/MDEditor.js
+++ src/component/page/MDEditor.js
@@ -1,71 +1,42 @@
-import React, {useState, useRef} from "react";
-import '@mdxeditor/editor/style.css'
+import { useState, useRef } from 'react'
 import { usePublisher } from '@mdxeditor/gurx'
-import { IoLogoYoutube } from "react-icons/io";
-import { CiYoutube } from "react-icons/ci";
-import Modal from "../common/Modal";
+import Modal from '../common/Modal'
 import i18next from 'i18next'
-
-import ko from './ko.json';
+import ko from './ko.json'
+import '@mdxeditor/editor/style.css'
 import './MDEditor.css'
+
+
+import { MDXEditor, codeMirrorPlugin, InsertSandpack, ShowSandpackInfo,ChangeAdmonitionType, imagePlugin, headingsPlugin, listsPlugin,
+  DiffSourceToggleWrapper, directivesPlugin, quotePlugin, InsertImage, thematicBreakPlugin, UndoRedo, CodeToggle, CreateLink, ListsToggle,
+  AdmonitionDirectiveDescriptor, BoldItalicUnderlineToggles, BlockTypeSelect, sandpackPlugin,  ChangeCodeMirrorLanguage, linkPlugin,
+  toolbarPlugin, linkDialogPlugin, insertDirective$, ConditionalContents, Separator, HighlightToggle, StrikeThroughSupSubToggles,
+  diffSourcePlugin, InsertTable, InsertThematicBreak, InsertCodeBlock, InsertFrontmatter, InsertAdmonition,
+  markdownShortcutPlugin, frontmatterPlugin, tablePlugin, codeBlockPlugin } from '@mdxeditor/editor'
+
 
 i18next.init({
   lng: 'ko',
-  fallbackLng: 'ko',  
+  fallbackLng: 'ko',
   resources: {ko: {translation: ko}}
 })
 
-import { MDXEditor, codeMirrorPlugin, InsertSandpack, ShowSandpackInfo,ChangeAdmonitionType, imagePlugin, headingsPlugin, listsPlugin, 
-  DiffSourceToggleWrapper, directivesPlugin, quotePlugin, InsertImage, thematicBreakPlugin, UndoRedo, CodeToggle, CreateLink, ListsToggle,
-  AdmonitionDirectiveDescriptor, BoldItalicUnderlineToggles, BlockTypeSelect, sandpackPlugin,  ChangeCodeMirrorLanguage, linkPlugin, LeafDirective,
-  toolbarPlugin, linkDialogPlugin, GenericDirectiveEditor, insertDirective$, ConditionalContents, Separator, HighlightToggle, StrikeThroughSupSubToggles, CodeMirrorEditor,
-  diffSourcePlugin, InsertTable, InsertThematicBreak, InsertCodeBlock, InsertFrontmatter, InsertAdmonition, DialogButton, useCodeBlockEditorContext,
-  markdownShortcutPlugin, CodeBlockEditor, NestedLexicalEditor, insertJsx$, GenericJsxEditor, jsxPlugin, DirectiveDescriptor, frontmatterPlugin, tablePlugin,
-  SandpackConfig, codeBlockPlugin, KitchenSinkToolbar, i18n } from '@mdxeditor/editor'
-
-
-
-const defaultSnippetContent = `
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      <h2>Input text</h2>
-    </div>
-  );
-}`
-
-
-
-const simpleSandpackConfig = {
-  defaultPreset: 'react',
-  presets: [
-    {
-      label: 'React',
-      name: 'react',
-      meta: 'live react',
-      sandpackTemplate: 'react',
-      sandpackTheme: 'light',
-      snippetFileName: '/App.js',
-      snippetLanguage: 'jsx',
-      initialSnippetContent: defaultSnippetContent
-    }
-  ]
-}
 
 
 export default function() {
 
   const ref = useRef(null)
 
-  const YoutubeDirectiveDescriptor  = {
+  const YoutubeDirectiveDescriptor = {
+
     name: 'youtube',
     type: 'leafDirective',
-    testNode(node) {      
+    testNode(node) {
       return node.name === 'youtube'
     },
     attributes: ['id'],
     hasChildren: false,
+
     Editor: ({ mdastNode, lexicalNode, parentEditor }) => {
       return (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -79,9 +50,8 @@ export default function() {
           >
           삭제
           </button>
-          <iframe width="560" height="315" src={`https://www.youtube.com/embed/${mdastNode.attributes.id}`} title="YouTube video player"
-            style={{ border: 0 }}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          <iframe width="560" height="315" src={`https://www.youtube.com/embed/${mdastNode.attributes.id}`} title="YouTube"
+            style={{ border: 0 }} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           ></iframe>
         </div>
       )
@@ -99,6 +69,7 @@ export default function() {
     
     return (<button onClick={() => {setIsModalOpen(true)}} title="유튜브 삽입">YT</button>)
   }
+
 
   const onYoutubeInput = (input) => {
 
@@ -139,6 +110,33 @@ export default function() {
     }
 
     return ['note', 'tip', 'danger', 'info', 'caution'].includes((node).getMdastNode().name)
+  }
+
+
+  const defaultSnippetContent = `
+    export default function App() {
+      return (
+        <div className="App">
+          <h1>Hello</h1>
+          <h2>Input text</h2>
+        </div>
+      );
+    }`
+    
+  const sandpackConfig = {
+    defaultPreset: 'react',
+    presets: [
+      {
+        label: 'React',
+        name: 'react',
+        meta: 'live react',
+        sandpackTemplate: 'react',
+        sandpackTheme: 'light',
+        snippetFileName: '/App.js',
+        snippetLanguage: 'jsx',
+        initialSnippetContent: defaultSnippetContent
+      }
+    ]
   }
 
 
@@ -196,8 +194,6 @@ export default function() {
     )
   }
 
-
-
   const markdown='test'
 
   const plugins = [
@@ -215,7 +211,7 @@ export default function() {
     thematicBreakPlugin(),
     frontmatterPlugin(),
     codeBlockPlugin({ defaultCodeBlockLanguage: 'js'}),    
-    sandpackPlugin({ sandpackConfig: simpleSandpackConfig }),
+    sandpackPlugin({ sandpackConfig: sandpackConfig }),
     codeMirrorPlugin({ codeBlockLanguages: { jsx:'react js', tsx:'react ts', js: 'java-script', ts: 'type-script', css: 'CSS', txt: 'plain text'} }),
     directivesPlugin({ directiveDescriptors: [YoutubeDirectiveDescriptor, AdmonitionDirectiveDescriptor] }),
     diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown: markdown }),
@@ -224,19 +220,11 @@ export default function() {
 
   const modal_config = {text: '유튜브 URL을 입력하세요', type: 'input', isCloseOutsideClick: true}
 
- 
-  
-
   return (
-      <div style={{
-      border: '1px solid #ccc',
-      borderRadius: '4px',
-      height: '400px', // Set your desired height
-      overflowY: 'auto', // Add vertical scrollbar if content overflows
-    }}>
+      <div style={{ border: '1px solid #ccc', borderRadius: '4px', height: '400px', overflowY: 'auto'}}>
       {/* <button onClick={() => ref.current?.setMarkdown('new markdown')}>Set new markdown</button>
       <button onClick={() => console.log(ref.current?.getMarkdown())}>Get markdown</button> */}
-      <MDXEditor ref={ref} markdown={markdown} onChange={console.log} readOnly={false} plugins={plugins} contentEditableClassName="prose" onError={(error) => {console.log('adfsadfasdf')}}      
+      <MDXEditor ref={ref} markdown={markdown} onChange={console.log} readOnly={false} plugins={plugins} contentEditableClassName="prose" onError={(error) => {console.log('adfsadfasdf')}}
         translation={(key, defaultValue, interpolations) => i18next.t(key, defaultValue, interpolations)}/>
       <Modal config={modal_config} isOpen={isModalOpen} onInput={onYoutubeInput} onClose={()=>setIsModalOpen(false)}></Modal>
       </div>

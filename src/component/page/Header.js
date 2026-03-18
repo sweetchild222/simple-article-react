00854840b13943d85a70a2771ae149bc38c9498c
inkuk <sweetchild22.ik@gmail.com>
2026-03-18 11:09:01 +0900

    'update'

--- src/component/page/Header.js
+++ src/component/page/Header.js
@@ -59,7 +59,7 @@ export default function() {
 
     return (
         <div style={{ display: 'flex', alignItems: 'center', height:'64px', backgroundColor: 'gray', paddingLeft:'30px'}}>
-        <img src='/image/logo.svg' alt='logo image' height='100%' width='64px' onClick={onClickHome}/>
+        <img src='/logo/logo.svg' alt='logo image' height='100%' width='64px' onClick={onClickHome}/>
         <div style={{flexGrow:1, backgroundColor:'green'}} ></div>
         <input id="myInput" placeholder="검색" style={{color:'green', width:'100px'}} onKeyDown={onKeyDown}></input>
         <button style={{backgroundColor:'red',  whiteSpace: 'nowrap', textAlign: 'center', flexGrow:0, margin:'10px', padding:'10px'}} onClick={onClickSearch}>검색</button>

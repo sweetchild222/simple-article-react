024fc7428a969fefc0a49179464a83547b4572f5
inkuk <sweetchild22.ik@gmail.com>
2026-07-03 15:35:44 +0900

    update

---
+++ src/page/main/ArticleItem.js
@@ -0,0 +1,84 @@
+import {useContext} from "react";
+import {useNavigate} from 'react-router-dom';
+import AuthContext from "@util/AuthContext.js";
+import ElapsedTime from "@util/ElapsedTime.js";
+import CountWithUnit from "@util/CountWithUnit.js";
+import StateProgsImage from "@gui/StateProgsImage.js";
+import * as ArticleAPI from '@rest/ArticleAPI.js'
+import { TiEye } from "react-icons/ti";
+import { MdThumbUpAlt } from "react-icons/md";
+import { MdThumbDownAlt } from "react-icons/md";
+import { BiSolidComment } from "react-icons/bi";
+import { IoMdHeart } from "react-icons/io";
+import {Vertical, Horizental} from "@gui/Flex.js";
+
+export default function({article, categoryName}) {
+
+    const {auth, updateAuth, validAuth, removeAuth} = useContext(AuthContext)
+    
+    const navigate = useNavigate()
+    
+    const onClickNavigateArticle = async() =>{
+
+        if(article.posted == 1)
+            navigate('article/' + article.id)
+        else{
+            
+            if(validAuth(auth)){
+
+                if(auth.blog_id != article.blog_id)
+                    return
+
+                const res = await ArticleAPI.getArticle(auth.jwt, article.id)
+
+                if(res.success == false){
+                    window.showToast('작성 중인 글을 가져 올 수 없습니다', 'error')
+                    return
+                }
+                navigate('/blog/' + article.blog_id + '/write', {state:res.payload})
+            }
+        }
+    }
+    
+    return (
+        <Horizental onClick={onClickNavigateArticle} style={{flex:'1', padding:'10px', cursor:'pointer', borderRadius:'3px', boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)', backgroundColor:'#F5F5F5'}}>
+            <Vertical style={{ flex:'1', marginLeft:'5px', marginRight:'5px'}}>
+                <div className={'clamped-text underline-text'} style={{'--line-count':2, fontSize:'18px', fontWeight:'600', marginBottom:'10px', color:'#1A1A1A'}}>{article.title != '' ? article.title : '...'}</div>
+                <div className={'clamped-text underline-text'} style={{'--line-count':3, marginBottom:'10px', color:'#222222'}}>{article.head.length >= 255 ? article.head + '...' : (article.head != '' ? article.head : '내용 없음')}</div>
+                <div style={{flex:'1'}}></div>
+                <Horizental style={{alignItems:'center', color:'#888888'}}>
+                    {article.posted == 1 &&                     
+                        <Horizental>
+                            <Horizental>
+                                <IoMdHeart size={22}/>
+                                <div style={{width:'5px'}}/>
+                                {CountWithUnit(article.bookmark_count)}
+                            </Horizental>
+                            <div style={{width:'30px'}}/>
+                            <Horizental>
+                                <MdThumbUpAlt size={22}/>
+                                <div style={{width:'5px'}}/>
+                                {CountWithUnit(article.like_count)}
+                            </Horizental>                            
+                            <div style={{width:'30px'}}/>
+                            <Horizental>
+                                <BiSolidComment size={22}/>
+                                <div style={{width:'5px'}}/>
+                                {CountWithUnit(article.comment_count)}
+                            </Horizental>
+                            <div style={{width:'30px'}}/>
+                        </Horizental>
+                    }
+                    {article.posted == 0 && <Horizental style={{marginRight:'30px'}}>
+                            <div className={'clamped-text'} style={{'--line-count':1, width:'160px'}}>{categoryName}</div>
+                        </Horizental>
+                    }
+                    <Horizental style={{flex:'1', whiteSpace: 'nowrap', justifyContent:'flex-end'}} >{article.post_at ? ElapsedTime(article.post_at) : ''}</Horizental>
+                </Horizental>
+
+            </Vertical>
+            {article.thumbnail != '' && <StateProgsImage src={article.thumbnail + '?size=170x170'} width={170} height={170} borderWidth={0}/>}
+        </Horizental>
+    )
+}
+
--- src/page/main/Home.js
+++ src/page/main/Home.js
@@ -1,117 +1,114 @@
+import {useState, useEffect, useContext, useRef } from "react";
+import {useNavigate, useLocation, useParams} from 'react-router-dom';
 
-import {useContext, useState, useRef} from 'react';
-import { useNavigate} from 'react-router-dom';
-
-import * as BlobAPI from '@rest/BlobAPI.js'
 import * as ArticleAPI from '@rest/ArticleAPI.js'
-import * as CommentAPI from '@rest/CommentAPI.js'
-import * as AlarmAPI from '@rest/AlarmAPI.js'
-import * as UserAPI from '@rest/UserAPI.js'
-import * as blobToBase64 from '@util/BlobToBase64.js'
 
-import PrettyButton from '@gui/PrettyButton.js'
 import AuthContext from "@util/AuthContext.js";
+import ArticleItem from "./ArticleItem.js";
 
-export default function Home() {
+import OverlayProgress from "@gui/OverlayProgress.js";
+import {Vertical, Horizental} from "@gui/Flex.js";
+import ToInteger from "@util/Integer.js";
+import './Home.css'
 
-  let inputEmail = null
-  let inputPassword = null
-  let verifyCode = null
+export default function() {
 
-  const refSelect = useRef(null)
-
-  const navigate = useNavigate();
-  const [isDisable, setIsDisable] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
-  const [isCreateExistModalOpen, setIsCreateExitModalOpen] = useState(false)
-  const [isModifyExistModalOpen, setIsModifyExitModalOpen] = useState(false)
-  const [article, setArticle] = useState(false)
-
+  
+  
+  const navigate = useNavigate()
+  
+  const location = useLocation()
+  
   const {auth, updateAuth, validAuth, removeAuth} = useContext(AuthContext)
+  const [articles, setArticles] = useState(null)
+  const [isOverlayProgress, setIsOverlayProgress] = useState(true)
+  const [reloadKey, setReloadKey] = useState(0)
 
+  const countPerPage = 8
 
-  const deleteComment = async() =>{
-
-    const comment_id = 9;
+  
+  const getBlogArticles = async(page, category_id, posted) => {
     
-    const res = await CommentAPI.deleteComment(auth.jwt, comment_id)
+    let query = 'offset=' + (countPerPage * page)
 
-    if(res.success == false)
-      return
+    query +=  '&limit=' + countPerPage
 
-    console.log(res.payload)
+    query += '&order=1'
+
+    query += category_id != null ? ('&category_id=' + category_id) : ''
+
+    query += posted != null ? ('&posted=' + posted) : ''
+    
+    const jwt = validAuth(auth.jwt) ? auth.jwt : null
+    
+    const res = await ArticleAPI.getBlogArticles(jwt, blog_id, query)
+
+    return res
+
   }
 
 
 
-  const getUser=async()=>{
 
-    const query = 'id=158,174,5'
-      
-    const res = await UserAPI.getUsers(query)
 
-    if(res.success == false)
-      return
+  const getCategoryName = (id) =>{
 
-    console.log(res.payload)
+    if(refCategories.current == null)
+      return ''
+
+    const categories = refCategories.current.categories()
+
+    const found = categories.find((item) => item.id == id)
+
+    if(found == null)
+      return ''
+
+    return found.name
   }
+  
 
+  const onClickPage = async(page) => {
 
-  const postAlarm = async()=>{
+    setIsOverlayProgress(true)    
 
-    const payload = {
+    const category_id = selectedCategory.id != 0 ? selectedCategory.id : null
+    const posted = selectedCategory.id != 0 ? null : 0
+        
+    const articles = await getBlogArticles(page, category_id, posted)
 
-      user_id:auth.user_id,
-      type:'ARTICLE',//'MENTION', 'COMMENT'
-      article_id:84,
-      comment_id:null
+    if(articles.success == true){      
+      setArticles(articles.payload)
     }
 
-    const res = await AlarmAPI.postAlarm(auth.jwt, payload)
-
-    console.log(res)
-
-  }
-
-
-  const getAlarm = async() =>{    
-
-    const res = await AlarmAPI.getAlarm(auth.jwt, auth.user_id)
-    console.log(res)
-  }
-
-  const deleteAlarm = async() =>{
-
-    const alarm_id = 12
-
-    const res = await AlarmAPI.deleteAlarm(auth.jwt, alarm_id)
-
-    console.log(res)
-    
-  }
-
-
-
-  const getArticles = async()=>{
-
-    const query = 'offset=0&limit=10&order_type=post_at&order=1'
-    //const query = 'offset=0&limit=10&order_type=like_count&order=1'
-    //const query = 'offset=0&limit=10&order_type=comment_count&order=1'
-    //const query = 'offset=0&limit=30&order_type=post_at&order=1&blog_id=29,30'
-
-    const res = await ArticleAPI.getArticles(query)
-
-    console.log(res.success)
-
-    console.log(res.payload)
-
+    setIsOverlayProgress(false)
   }
 
   
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height:'100%', marginTop:'40px'}}>  
-      <PrettyButton disabled={false} isLoading={isLoading} type='success' onClick={getArticles}>게시글 가져오기</PrettyButton>      
-            
-    </div>
-  );  
+      <Horizental style={{width:'100%'}}>
+        <div style={{width:'128px'}}/>
+        <div style={{flex:'1', position:'relative'}}>
+          <Vertical>
+              {articles && (
+                articles.length > 0 ? 
+                (<Vertical style={{width:'100%'}}>
+                  <div className={'dynamicColumnContainer'} style={{width:'100%', marginTop:'8px', marginBottom:'16px'}}>
+                    {articles.map((data, index) => <ArticleItem key={data.id} article={data} categoryName={getCategoryName(data.category_id)}/>)}
+                  </div>
+                  <Horizental style={{width:'100%'}}>
+                    {/* {selectedCategory.article_count > countPerPage && <Pagination key={reloadKey} totalPageCount={Math.ceil(selectedCategory.article_count / countPerPage)} displayPageCount={3} onClickPage={onClickPage}/>} */}
+                    <div style={{flex:'1'}}></div>
+                  </Horizental>
+                </Vertical>) : 
+                (<Vertical style={{alignItems:'center', width:'100%', justifyContent:'center', height:'100%', marginTop:'128px'}}>
+                  {<img src={'/image/empty.png'} style={{width:'128px', height: '128px'}}/>}
+                  {<div style={{fontSize:'18px', marginTop:'32px', marginBottom:'32px'}}>{'글이 없습니다.'}</div>}                  
+                </Vertical>)
+              )}
+          </Vertical>
+          {isOverlayProgress && <OverlayProgress type={'absolute'}/>}
+        </div>        
+        <div style={{width:'128px'}}/>
+      </Horizental>
+  )
 }

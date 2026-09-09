4ff75963382cecc1388fa394f80284387bd2e0bf
inkuk <sweetchild22.ik@gmail.com>
2026-09-09 12:38:24 +0900

    'update'

--- src/page/common/ErrorCatch.js
+++ src/page/common/ErrorCatch.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 import {Vertical} from "@gui/Flex.js";
-import { useTranslation } from 'react-i18next';
-
-
 
 class ErrorCatch extends React.Component {
 
   constructor(props) {
     
     super(props)
-    this.state = { hasError: false }
-    
+    this.state = { hasError: false }    
   }
 
   static getDerivedStateFromError(error) {
@@ -32,12 +28,10 @@ class ErrorCatch extends React.Component {
 
     if (this.state.hasError) {
 
-      const { t } = useTranslation()
-
       return (
               <Vertical style={{alignItems: 'center'}}>
-                <h1>{t('page.common.errorOccured')}</h1>
-                <h1>{t('page.common.contactSiteAdminisrator')}</h1>
+                <h1>{'Sorry, Aplication has error'}</h1>
+                <h1>{'Please contact site adminisrator'}</h1>
               </Vertical>
             )
     }
